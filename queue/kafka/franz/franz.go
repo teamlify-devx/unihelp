3792cf4281537c44b67cc04d4001f4f4f24606eb
Sercan REYHANLI <sercan@teamlify.co.uk>
2026-04-21 03:11:48 +0300

    feat: update Kafka client configuration to enable consumer support and add topic consumption

--- queue/kafka/franz/franz.go
+++ queue/kafka/franz/franz.go
@@ -11,33 +11,39 @@ import (
 // NewKafkaClient Return new Kafka client instance
 func NewKafkaClient(ctx context.Context) (client *kgo.Client, err error) {
 	opts := []kgo.Opt{
-		kgo.SeedBrokers(cfg.GetStringSlice("Kafka.BROKERS")...),
-		kgo.ClientID(cfg.GetString("Kafka.CLIENT_ID")),
-		kgo.RequestTimeoutOverhead(cfg.GetDuration("Kafka.REQUEST_TIMEOUT_OVERHEAD") * time.Second),
-		kgo.ConnIdleTimeout(cfg.GetDuration("Kafka.CONNECTION_IDLE_TIMEOUT") * time.Second),
+		kgo.SeedBrokers(cfg.GetStringSlice("kafka.BROKERS")...),
+		kgo.ClientID(cfg.GetString("kafka.CLIENT_ID")),
+		kgo.RequestTimeoutOverhead(cfg.GetDuration("kafka.REQUEST_TIMEOUT_OVERHEAD") * time.Second),
+		kgo.ConnIdleTimeout(cfg.GetDuration("kafka.CONNECTION_IDLE_TIMEOUT") * time.Second),
 		kgo.ProducerBatchCompression(kgo.SnappyCompression()),
-		kgo.ProducerBatchMaxBytes(cfg.GetInt32("Kafka.PRODUCER_BATCH_MAX_BYTES")),
-		kgo.ProducerLinger(cfg.GetDuration("Kafka.PRODUCER_LINGER") * time.Millisecond),
+		kgo.ProducerBatchMaxBytes(cfg.GetInt32("kafka.PRODUCER_BATCH_MAX_BYTES")),
+		kgo.ProducerLinger(cfg.GetDuration("kafka.PRODUCER_LINGER") * time.Millisecond),
 		kgo.RequiredAcks(kgo.AllISRAcks()),
-		kgo.MaxConcurrentFetches(cfg.GetInt("Kafka.MAX_CONCURRENT_FETCHES")),
-		kgo.ConsumerGroup(cfg.GetString("Kafka.CONSUMER_GROUP")),
+		kgo.MaxConcurrentFetches(cfg.GetInt("kafka.MAX_CONCURRENT_FETCHES")),
 	}
 
-	if cfg.GetBool("Kafka.ALLOW_AUTO_TOPIC_CREATION") == true {
+	if cfg.GetBool("kafka.ALLOW_AUTO_TOPIC_CREATION") == true {
 		opts = append(opts,
 			kgo.AllowAutoTopicCreation(),
 		)
 	}
 
-	if cfg.GetBool("Kafka.ALLOW_CONSUMER_GROUP") == true {
+	if cfg.GetBool("kafka.ENABLE_CONSUMER") == true {
 		opts = append(opts,
-			kgo.ConsumerGroup(cfg.GetString("Kafka.CONSUMER_GROUP")),
+			kgo.ConsumerGroup(cfg.GetString("kafka.CONSUMER_GROUP")),
+			kgo.ConsumeTopics(cfg.GetString("kafka.TOPICS")),
+		)
+	}
+
+	if cfg.GetBool("kafka.DISABLE_AUTO_COMMIT") == true {
+		opts = append(opts,
+			kgo.DisableAutoCommit(),
 		)
 	}
 
 	client, err = kgo.NewClient(opts...)
 
-	ctx, cancel := context.WithTimeout(ctx, cfg.GetDuration("Kafka.CONN_TIMEOUT")*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, cfg.GetDuration("kafka.CONN_TIMEOUT")*time.Second)
 	defer cancel()
 
 	if err = client.Ping(ctx); err != nil {
